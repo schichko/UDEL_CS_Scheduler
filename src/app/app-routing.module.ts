5599b14bba266779fed9539e595b3939e009c55a
Will Walker <wwalker@udel.edu>
2019-10-21 10:03:35 -0400

    use the production flag to enable cas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,28 +3,26 @@ import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { TermComponent } from './term/term.component'
 import { PlanComponent } from './plan/plan.component'
-
-
-// UNCOMMENT AND USE THIS WHEN IN PRODUCTION FOR AUTHENTICATION
-/*
+import { environment } from 'src/environments/environment'
 import { AuthGuard } from './auth.guard'
-const routes: Routes = [
-  { path: 'term', component: TermComponent, canActivate: [AuthGuard] },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'plan/:planID', component: PlanComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] }
-];
-*/
 
-// USE THIS WHEN IN DEV
+let routes: Routes = [];
 
-const routes: Routes = [
-  { path: 'term', component: TermComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'plan/:planID', component: PlanComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
-];
-
+if (environment.production) {
+  routes = [
+    { path: 'term', component: TermComponent, canActivate: [AuthGuard] },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'plan/:planID', component: PlanComponent, canActivate: [AuthGuard] },
+    { path: '', redirectTo: '/home', pathMatch: 'full', canActivate: [AuthGuard] }
+  ];
+} else {
+  routes = [
+    { path: 'term', component: TermComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'plan/:planID', component: PlanComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+  ];
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
